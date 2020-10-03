751d1684ab4fa6be01d3321411c07d0b27516768
Alex Ionescu <alxi.2001@gmail.com>
2020-10-03 19:24:25 +0300

    Build docs automatically

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-dune build \
-&& dune build @doc \
-&& rm -rf docs/docs/* \
-&& cp -a _build/default/_doc/_html/. docs/docs/
---
+++ copy-docs.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+rm -rf docs/docs/*
+cp -a _build/default/_doc/_html/. docs/docs/
