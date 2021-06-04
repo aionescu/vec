3d59a6f23adb14aad2aaf0b6d86f8c7a3b9a9a4b
Alex Ionescu <alxi.2001@gmail.com>
2021-06-05 02:19:53 +0300

    Reorganize docs

--- src/vec.mli
+++ src/vec.mli
@@ -9,6 +9,8 @@ type ('a, -'p) t
 val default_growth_rate: float
 (** The default growth rate of newly-created vectors. *)
 
+(** {1 Creating vectors} *)
+
 val make: ?growth_rate:float -> ?capacity:int -> unit -> ('a, [`R | `W]) t
 (** Constructs a vector with the specified growth rate and capacity.
 
@@ -16,8 +18,16 @@ val make: ?growth_rate:float -> ?capacity:int -> unit -> ('a, [`R | `W]) t
 
     Capacity defaults to 0. *)
 
+val range: int -> int -> (int, [`R | `W]) t
+(** Constructs a vector containing all numbers in the specified range. *)
+
+val singleton: 'a -> ('a, [`R | `W]) t
+(** Returns a singleton vector containing the specified item. (Applicative functor [pure] operation) *)
+
 val copy: ('a, [> `R]) t -> ('a, [`R | `W]) t
 (** Creates a copy of the vector. *)
+
+(** {1 Restricting permissions} *)
 
 val as_read_only: ('a, [> `R]) t -> ('a, [`R]) t
 (** Reinterprets the vector as a read-only vector. *)
@@ -25,11 +35,16 @@ val as_read_only: ('a, [> `R]) t -> ('a, [`R]) t
 val as_write_only: ('a, [> `W]) t -> ('a, [`W]) t
 (** Reinterprets the vector as a write-only vector. *)
 
+(** {1 Basic properties} *)
+
 val length: ('a, [>]) t -> int
 (** Returns the length of the vector. *)
 
 val capacity: ('a, [>]) t -> int
 (** Returns the capacity of the vector. *)
+
+val ensure_capacity: int -> ('a, [> `W]) t -> unit
+(** Ensures the vector's buffer has at least as many free slots as the specified value. *)
 
 val growth_rate: ('a, [>]) t -> float
 (** Returns the growth rate of the vector. *)
@@ -40,8 +55,7 @@ val set_growth_rate: float -> ('a, [> `W]) t -> unit
 val ensure_growth_rate: float -> ('a, [> `W]) t -> unit
 (** Ensures the vector's growth rate is at least as great as the specified value. *)
 
-val clear: ('a, [> `W]) t -> unit
-(** Resets the vector to an empty state. *)
+(** {1 Indexing} *)
 
 val get_exn: ('a, [> `R]) t -> int -> 'a
 (** Gets the value in the vector at the specified index.
@@ -57,59 +71,7 @@ val get: ('a, [> `R]) t -> int -> 'a option
 val set: ('a, [> `W]) t -> int -> 'a -> bool
 (** Sets the value in the vector at the specified index to the specified value. Returns false if the index is out of range. *)
 
-val ensure_capacity: int -> ('a, [> `W]) t -> unit
-(** Ensures the vector's buffer has at least as many free slots as the specified value. *)
-
-val shrink_to_fit: ('a, [> `W]) t -> unit
-(** Shrinks the vector's internal buffer to only be as large as the vector's length. *)
-
-val push: 'a -> ('a, [> `W]) t -> unit
-(** Pushes the specified item onto the end of the vector. *)
-
-val pop: ('a, [`R | `W]) t -> 'a option
-(** Pops off the item from the end of the vector. *)
-
-val map: ('a -> 'b) -> ('a, [> `R]) t -> ('b, [`R | `W]) t
-(** Maps the specified function over the vector, returning a new vector. (Functor [map] operation) *)
-
-val mapi: (int -> 'a -> 'b) -> ('a, [> `R]) t -> ('b, [`R | `W]) t
-(** Like {!map}, but the function also takes the item's index as a parameter. *)
-
-val map_in_place: ('a -> 'a) -> ('a, [`R | `W]) t -> unit
-(** Like {!map}, but the transformation is done in-place. *)
-
-val singleton: 'a -> ('a, [`R | `W]) t
-(** Returns a singleton vector containing the specified item. (Applicative functor [pure] operation) *)
-
-val map2: ('a -> 'b -> 'c) -> ('a, [> `R]) t -> ('b, [> `R]) t -> ('c, [`R | `W]) t
-(** Maps the specified function over all combinations of tuples from the 2 vectors, returning a new vector. (Applicative functor [liftA2] operation *)
-
-val apply: ('a -> 'b, [> `R]) t -> ('a, [> `R]) t -> ('b, [`R | `W]) t
-(** Applies every function from the first vector to every value from the second vector, returning a new vector. (Applicatve functor [ap] operation) *)
-
-val flatten: (('a, [> `R]) t, [> `R]) t -> ('a, [`R | `W]) t
-(** Flattens nested vectors into a single, one-dimensional vector. (Monad [join] operation) *)
-
-val flat_map: ('a -> ('b, [> `R]) t) -> ('a, [> `R]) t -> ('b, [`R | `W]) t
-(** Like {!map}, but flattens the result. (Monad [bind] operation) *)
-
-val cartesian_product: ('a, [> `R]) t -> ('b, [> `R]) t -> ('a * 'b, [`R | `W]) t
-(** Cartesian product of 2 vectors. (Equivalent to [liftA2 (,)]) *)
-
-val iter: ('a -> unit) -> ('a, [> `R]) t -> unit
-(** Applies the specified function to each item in the vector. *)
-
-val iteri: (int -> 'a -> unit) -> ('a, [> `R]) t -> unit
-(** Like {!iter}, but the function also takes the item's index as a parameter. *)
-
-val filter: ('a -> bool) -> ('a, [> `R]) t -> ('a, [`R | `W]) t
-(** Returns a new vector containing only the items from the first vector that satisfy the specified predicate. *)
-
-val filteri: (int -> 'a -> bool) -> ('a, [> `R]) t -> ('a, [`R | `W]) t
-(** Like {!filter}, but the predicate also takes the item's index as a parameter. *)
-
-val filter_in_place: ('a -> bool) -> ('a, [`R | `W]) t -> unit
-(** Performs a filter in-place, based on the specified predicate. *)
+(** {1 Conversions} *)
 
 val of_list: 'a list -> ('a, [`R | `W]) t
 (** Constructs a vector from the specified list. *)
@@ -123,17 +85,87 @@ val of_array: 'a array -> ('a, [`R | `W]) t
 val to_array: ('a, [> `R]) t -> 'a array
 (** Constructs an array from the specified vector. *)
 
+val pretty_print: ('a -> string) -> ('a, [> `R]) t -> string
+(** Returns a string representation of the vector, using the specified function to format each value. *)
+
+(** {1 Modifying vectors} *)
+
+val clear: ('a, [> `W]) t -> unit
+(** Resets the vector to an empty state. *)
+
+val shrink_to_fit: ('a, [> `W]) t -> unit
+(** Shrinks the vector's internal buffer to only be as large as the vector's length. *)
+
+val push: 'a -> ('a, [> `W]) t -> unit
+(** Pushes the specified item onto the end of the vector. *)
+
+val pop: ('a, [`R | `W]) t -> 'a option
+(** Pops off the item from the end of the vector. *)
+
+(** {1 Transformations} *)
+
+val map: ('a -> 'b) -> ('a, [> `R]) t -> ('b, [`R | `W]) t
+(** Maps the specified function over the vector, returning a new vector. (Functor [map] operation) *)
+
+val mapi: (int -> 'a -> 'b) -> ('a, [> `R]) t -> ('b, [`R | `W]) t
+(** Like {!map}, but the function also takes the item's index as a parameter. *)
+
+val map_in_place: ('a -> 'a) -> ('a, [`R | `W]) t -> unit
+(** Like {!map}, but the transformation is done in-place. *)
+
+val flat_map: ('a -> ('b, [> `R]) t) -> ('a, [> `R]) t -> ('b, [`R | `W]) t
+(** Like {!map}, but flattens the result. (Monad [bind] operation) *)
+
 val rev: ('a, [> `R]) t -> ('a, [`R | `W]) t
 (** Returns a new vector that contains all the items in the specified vector, but in reverse order. *)
 
 val rev_in_place: ('a, [`R | `W]) t -> unit
 (** Reverses the vector in-place. *)
 
+(** {1 Filters & folds} *)
+
+val filter: ('a -> bool) -> ('a, [> `R]) t -> ('a, [`R | `W]) t
+(** Returns a new vector containing only the items from the first vector that satisfy the specified predicate. *)
+
+val filteri: (int -> 'a -> bool) -> ('a, [> `R]) t -> ('a, [`R | `W]) t
+(** Like {!filter}, but the predicate also takes the item's index as a parameter. *)
+
+val filter_in_place: ('a -> bool) -> ('a, [`R | `W]) t -> unit
+(** Performs a filter in-place, based on the specified predicate. *)
+
+val fold_left: ('b -> 'a -> 'b) -> 'b -> ('a, [> `R]) t -> 'b
+(** Folds the specified function and default value over the array, from left to right. *)
+
+val fold_right: ('a -> 'b -> 'b) -> ('a, [> `R]) t -> 'b -> 'b
+(** Folds the specified function and default value over the array, from right to left. *)
+
+(** {1 Combining vectors} *)
+
 val append: ('a, [> `R]) t -> ('a, [> `R]) t -> ('a, [`R | `W]) t
 (** Concatenates the two vectors into a new vector. *)
 
 val append_in_place: ('a, [`R | `W]) t -> ('a, [> `R]) t -> unit
 (** Appends the second vector to the first vector in-place. *)
+
+val cartesian_product: ('a, [> `R]) t -> ('b, [> `R]) t -> ('a * 'b, [`R | `W]) t
+(** Cartesian product of 2 vectors. (Equivalent to [liftA2 (,)]) *)
+
+val map2: ('a -> 'b -> 'c) -> ('a, [> `R]) t -> ('b, [> `R]) t -> ('c, [`R | `W]) t
+(** Maps the specified function over all combinations of tuples from the 2 vectors, returning a new vector. (Applicative functor [liftA2] operation *)
+
+val apply: ('a -> 'b, [> `R]) t -> ('a, [> `R]) t -> ('b, [`R | `W]) t
+(** Applies every function from the first vector to every value from the second vector, returning a new vector. (Applicatve functor [ap] operation) *)
+
+val flatten: (('a, [> `R]) t, [> `R]) t -> ('a, [`R | `W]) t
+(** Flattens nested vectors into a single, one-dimensional vector. (Monad [join] operation) *)
+
+val zip: ('a, [> `R]) t -> ('b, [> `R]) t -> ('a * 'b, [`R | `W]) t
+(** Zips the two vectors together. *)
+
+val zip_with: ('a -> 'b -> 'c) -> ('a, [> `R]) t -> ('b, [> `R]) t -> ('c, [`R | `W]) t
+(** Zips the two vectors together, using the specified function to combine values. *)
+
+(** {1 Querying vectors} *)
 
 val exists: ('a -> bool) -> ('a, [> `R]) t -> bool
 (** Returns [true] if any item in the vector satisfies the specified predicate. *)
@@ -147,23 +179,15 @@ val mem: 'a -> ('a, [> `R]) t -> bool
 val memq: 'a -> ('a, [> `R]) t -> bool
 (** Returns [true] if the specified item exists in the vector. Uses physical equality. *)
 
-val fold_left: ('b -> 'a -> 'b) -> 'b -> ('a, [> `R]) t -> 'b
-(** Folds the specified function and default value over the array, from left to right. *)
-
-val fold_right: ('a -> 'b -> 'b) -> ('a, [> `R]) t -> 'b -> 'b
-(** Folds the specified function and default value over the array, from right to left. *)
-
-val zip: ('a, [> `R]) t -> ('b, [> `R]) t -> ('a * 'b, [`R | `W]) t
-(** Zips the two vectors together. *)
-
-val zip_with: ('a -> 'b -> 'c) -> ('a, [> `R]) t -> ('b, [> `R]) t -> ('c, [`R | `W]) t
-(** Zips the two vectors together, using the specified function to combine values. *)
+(** {1 Sorting} *)
 
 val sort: ('a, [`R | `W]) t -> unit
 (** Sorts the vector. *)
 
 val sort_by: ('a -> 'a -> int) -> ('a, [`R | `W]) t -> unit
 (** Sorts the vector using the specified comparison function. *)
+
+(** {1 Equality and comparison} *)
 
 val equal: ('a, [> `R]) t -> ('a, [> `R]) t -> bool
 (** Compares two vectors for equality. *)
@@ -177,11 +201,15 @@ val compare: ('a, [> `R]) t -> ('a, [> `R]) t -> int
 val compare_by: ('a -> 'a -> int) -> ('a, [> `R]) t -> ('a, [> `R]) t -> int
 (** Compares two vectors lexicographically, using the specified comparison function for elements. *)
 
-val pretty_print: ('a -> string) -> ('a, [> `R]) t -> string
-(** Returns a string representation of the vector, using the specified function to format each value. *)
+(** {1 Iteration} *)
 
-val range: int -> int -> (int, [`R | `W]) t
-(** Constructs a vector containing all numbers in the specified range. *)
+val iter: ('a -> unit) -> ('a, [> `R]) t -> unit
+(** Applies the specified function to each item in the vector. *)
+
+val iteri: (int -> 'a -> unit) -> ('a, [> `R]) t -> unit
+(** Like {!iter}, but the function also takes the item's index as a parameter. *)
+
+(** {1 Infix operators} *)
 
 module Infix: sig
   val (.![]): ('a, [> `R]) t -> int -> 'a
@@ -221,6 +249,8 @@ module Infix: sig
   (** Infix version of {!range}. *)
 end
 (** Contains infix versions of some vector operations. *)
+
+(** {1 Monadic syntax} *)
 
 module Let_syntax: sig
   val (let+): ('a, [> `R]) t -> ('a -> 'b) -> ('b, [`R | `W]) t
